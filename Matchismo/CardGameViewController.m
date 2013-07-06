3cfedd0d4b319c86f660f0a765611fea2119a21a
Mark Nichols <mark@zanshin.net>
2013-07-06 12:56:49 -0500

    Completed assignment 0: Randomized cards as they are flipped over

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,23 +7,37 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation CardGameViewController
+
+// Lazily instantiate our Deck
+- (Deck *)deck
+{
+    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
 
 - (void)setFlipCount:(int)flipCount
 {
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    
 }
 
 - (IBAction)flipCard:(UIButton *)sender
 {
     sender.selected = !sender.isSelected;
+    
+    Card *card = [self.deck drawRandomCard];
+    [sender setTitle:card.contents forState:UIControlStateSelected];
+    
     self.flipCount++;
 }
 
