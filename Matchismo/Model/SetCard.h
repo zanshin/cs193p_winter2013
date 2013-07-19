db8cf720aa3aa65e5d7e4634231c8cd306c741ed
Mark Nichols <mark@zanshin.net>
2013-07-19 14:12:08 -0500

    Added SetCard and SetCardDeck classes.
    SetCard subclasses Card
    SetCardDeck subclasses Deck
    Both will mirror their playing card peers

---
+++ Matchismo/Model/SetCard.h
@@ -0,0 +1,13 @@
+//
+//  SetCard.h
+//  Matchismo
+//
+//  Created by Mark Nichols on 7/19/13.
+//  Copyright (c) 2013 Mark Nichols. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface SetCard : Card
+
+@end
---
+++ Matchismo/Model/SetCard.m
@@ -0,0 +1,13 @@
+//
+//  SetCard.m
+//  Matchismo
+//
+//  Created by Mark Nichols on 7/19/13.
+//  Copyright (c) 2013 Mark Nichols. All rights reserved.
+//
+
+#import "SetCard.h"
+
+@implementation SetCard
+
+@end
