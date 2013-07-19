ef842b92d13f4d2ad238e808fecc54dbbf3a01a2
Mark Nichols <mark@zanshin.net>
2013-07-19 13:00:41 -0500

    Added SetGameViewController and UITabBarController to storyboard.
    Added SetGameViewController for new Set game
    Added UITabBarController to storyboard, removed default scenes, wired
    CardGameViewController as first tab and SetGameViewController as second tab.
    
    Moved celloCardback.jpg to Supporting Files group.

---
+++ Matchismo/SetGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.h
+//  Matchismo
+//
+//  Created by Mark Nichols on 7/19/13.
+//  Copyright (c) 2013 Mark Nichols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetGameViewController : UIViewController
+
+@end
